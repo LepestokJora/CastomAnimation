c15f77578a7891a7e64245e0c36cf1640fd8bd7f
Artem Peshkov <pewkoff@gmail.com>
2023-01-29 23:27:49 +0300

    fix

--- CastomAnimation/ContentView.swift
+++ CastomAnimation/ContentView.swift
@@ -5,42 +5,75 @@
 //  Created by Lepestok Jora on 29.01.2023.
 //
 
-import SwiftUI
+        import SwiftUI
 
-struct ContentView: View {
-    
-    var body: some View {
+        struct ContentView: View {
+
+
+        var body: some View {
         ZStack{
-            Rectangle()
-                .fill(Color.green)
-                .cornerRadius(20)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 20)
-                        .stroke(.black, lineWidth: 4))
-                .shadow(color: .green, radius: 20 )
-            HStack{
-                Image(systemName: "figure.run")
-                    .resizable()
-                    .frame(width: 130, height: 130)
-                    .rotationEffect(.degrees(0))
-                    .colorInvert()
-                    
-                    .padding()
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack{
                 
-                Rectangle()
-                    .frame(width: 80, height: 150)
-                    .colorInvert()
-                    .shadow(color: .white, radius: 20 )
+                Text("SwiftUI")
+                    .font(.system(size: 50))
+                    .bold()
+                    .foregroundColor(.white)
+                    .shadow(color: .white, radius: 5)
+                    .padding(80)
+                ZStack{
+                    
+                    
+                    Rectangle()
+                        .fill(Color.green)
+                        .cornerRadius(20)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(.white, lineWidth: 4))
+                        .shadow(color: .green, radius: 30 )
+                        .frame(width: 300, height: 200)
+                    HStack{
+                        
+                        Image(systemName: "figure.run")
+                            .resizable()
+                            .frame(width: 130, height: 130)
+                            .rotationEffect(.degrees(0))
+                            .colorInvert()
+                        
+                            .padding()
+                        
+                        Rectangle()
+                            .frame(width: 80, height: 150)
+                            .colorInvert()
+                            .shadow(color: .white, radius: 20 )
+                    }
+                    
+                }
+                .padding(60)
+                Text("Go..")
+                    .font(.system(size: 50))
+                    .bold()
+                    .foregroundColor(.white)
+                    .shadow(color: .white, radius: 5)
+                Spacer()
+                
             }
+            
         }
+            
     }
-    
+            
+            
 }
-    
+
     struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-                .frame(width: 300, height: 200)
-        }
+    static var previews: some View {
+        ContentView()
+        
+        
     }
+
+}
 
